b8985f79f81879e94d874a1ffaf8a7d03d36c160
restobit <restobit@protonmail.com>
2022-04-01 14:23:14 +0200

    Removed all console logs and separated functions in posts.js

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ import PostCard from "../components/PostCard";
 import styles from "../styles/Home.module.css";
 
 export default function Home({ posts }) {
-  console.log("posts", posts);
   return (
     <div>
       <Head>
@@ -36,18 +35,11 @@ export async function getServerSideProps(ctx) {
   let dev = process.env.NODE_ENV !== "production";
   let { DEV_URL, PROD_URL } = process.env;
 
-  console.log("dev", dev);
-  console.log("DEV_URL", DEV_URL);
-  console.log("PROD_URL", PROD_URL);
-  console.log("URL", `${dev ? DEV_URL : PROD_URL}/api/posts`);
-
   // request posts from api
   let res = await fetch(`${dev ? DEV_URL : PROD_URL}/api/posts`);
 
-  console.log("res ", res);
   // extract the data
   let data = await res.json();
-  console.log("data", data);
 
   return {
     props: {
